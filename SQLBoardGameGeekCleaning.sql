b9546d944db5692d12d20b32598af976d06354dc
WinaMerican <148327027+WinaMerican@users.noreply.github.com>
2023-12-18 20:47:58 +0800

    Add files via upload

---
+++ SQLBoardGameGeekCleaning.sql
@@ -0,0 +1,151 @@
+Select * 
+From dbo.Games
+Order by BGGId
+
+--check for any NULL
+Select *
+from dbo.Games
+where Name is NULL
+
+Select *
+from dbo.Games
+where YearPublished is NULL
+
+Select *
+from dbo.Games
+where AvgRating is NULL
+
+Select *
+from dbo.Games
+where MinPlayers is NULL
+
+Select *
+from dbo.Games
+where MaxPlayers is NULL
+
+Select *
+from dbo.Games
+where Family is NULL
+
+--set NULL to Not Specified for family
+Update dbo.Games
+Set Family = 'Not Specified'
+Where Family is NULL
+
+--set recommended age to whole number
+Alter Table Games
+add RecAge float;
+
+Update Games
+set RecAge = cast (ComAgeRec as decimal(10,0));
+
+--Replace RecAge Null with Recommended age from manufacturer
+Update dbo.Games
+Set RecAge = MfgAgeRec
+Where RecAge is Null
+
+--identify min and max recommended age
+Select min(RecAge)
+from Games
+
+Select max(RecAge)
+from Games
+
+--set GameWeight to 2 decimal point
+Alter Table Games
+add GameWeightDec float;
+
+Update Games
+set GameWeightDec = cast (GameWeight as decimal(10,2));
+
+--to deteremine the range for gameweight
+Select min(GameWeightDec)
+from Games
+
+Select max(GameWeightDec)
+from Games
+-- 0 being the easiest, 5 the hardest
+
+--set Average Rating to 2 decimal point
+Alter Table Games
+add AvgRate float;
+
+Update Games
+set AvgRate = cast (AvgRating as decimal(10,2));
+
+--to deteremine the range for gameweight
+Select min(AvgRate)
+from Games
+
+Select max(AvgRate)
+from Games
+-- 1.04 lowesst rating, 9.91 highest rating
+
+--Min player
+Select min(MinPlayers)
+from Games
+--min player = 0, need to dive into the games
+Select * 
+From dbo.Games
+Where MinPlayers = 0
+--a lot of games with 0 min and 0 max player
+--game with no specified number of players
+
+Select max(MaxPlayers)
+from Games
+--max players = 999 people
+
+--determine the earliest and latest game
+Select min(YearPublished)
+from Games
+--there are games from 3500 B.C, to check the games B.C
+Select *
+From Games
+Where YearPublished <= 0
+
+Select max(YearPublished)
+from Games
+--latest game is 2021 from the dataset
+
+--To drop columns that we dont need
+Alter Table Games
+Drop Column Description, BayesAvgRating, StdDev, ComAgeRec, BestPlayers, GoodPlayers, NumWeightVotes, MfgPlaytime, MfgAgeRec, NumUserRatings, NumComments, NumAlternates, NumExpansions, NumImplementations, IsReimplementation, Kickstarted, ImagePath;
+
+Alter Table Games
+Drop Column LanguageEase, AvgRating, GameWeight;
+
+Select Count(DISTINCT [Rank:boardgame]) as rank_board_dist,
+	   Count([Rank:boardgame]) as rank_boardgame,
+	   Count(DISTINCT [Rank:strategygames]) as rank_strategy_dist,
+	   Count([Rank:strategygames]) as rank_strategy,
+	   Count(DISTINCT [Rank:abstracts]) as rank_abstract_dist,
+	   Count([Rank:abstracts]) as rank_abstract,
+	   Count(DISTINCT [Rank:familygames]) as rank_family_dist,
+	   Count([Rank:familygames]) as rank_family,
+	   Count(DISTINCT [Rank:thematic]) as rank_thematic_dist,
+	   Count([Rank:thematic]) as rank_thematic,
+	   Count(DISTINCT [Rank:cgs]) as rank_cgs_dist,
+	   Count([Rank:cgs]) as rank_cgs,
+	   Count(DISTINCT [Rank:wargames]) as rank_war_dist,
+	   Count([Rank:wargames]) as rank_war,
+	   Count(DISTINCT [Rank:partygames]) as rank_party_dist,
+	   Count([Rank:partygames]) as rank_party,
+	   Count(DISTINCT [Rank:childrensgames]) as rank_children_dist,
+	   Count([Rank:childrensgames]) as rank_children
+From Games
+--there are more than 1 games in a ranking, some games share ranks
+
+Select *
+From Games
+Order by BGGId
+
+Select *
+From Games
+Where [Rank:boardgame] <= 50
+Order by [Rank:boardgame]
+
+Select *
+From dbo.Games
+Inner Join dbo.subcategories$
+On dbo.Games.BGGId = dbo.subcategories$.BGGId
+
